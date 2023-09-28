c45f24f97e6fa9c0d2635fda08c18bbe7f8c0e97
Raul Low <raulou92@gmail.com>
2023-09-28 01:14:41 -0300

    adding some text on proyectos component

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -2,11 +2,10 @@ const Proyectos = () => {
     return (
         <a
         href="#"
-        target="_blank"
-        rel="noopener noreferrer"
         className="flex flex-col justify-center order-9 md:order-8 items-center rounded-[2rem] bg-[#ececec] dark:bg-neutral-900 text-center text-neutral-600 dark:text-neutral-300 aspect-square"
     >
-        <p className="text-xl font-medium">Proyectos</p>
+        <p className="lg:text-2xl font-medium">Proyectos</p>
+        <p className="lg:text-md font-light">pronto</p>
     </a>
     )
 }
