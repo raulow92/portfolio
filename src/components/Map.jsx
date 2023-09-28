c48c8877bce825d46c64ef0e0361093f7aa97e7d
Raul Low <raulou92@gmail.com>
2023-09-28 11:19:43 -0300

    changing map url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ import { TbMapPin } from "react-icons/tb";
 const Map = () => {
     return (
     <a
-        href="https://maps.app.goo.gl/sqpKZjLscjeomaNz8"
+        href="https://maps.app.goo.gl/kstjLtETwSQP3pRE7"
         target="_blank"
         rel="noopener noreferrer"
         className="flex flex-col relative justify-center order-6 items-center rounded-[2rem] bg-[#ececec] dark:bg-neutral-900 text-center text-neutral-600 dark:text-neutral-300 aspect-square"
