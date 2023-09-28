32f9b945f9495cd6dfbff6242e6bbd7046161542
Raul Low <raulou92@gmail.com>
2023-09-28 12:34:54 -0300

    changing favicon again

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -5,7 +5,7 @@ const Proyectos = () => {
         className="flex flex-col justify-center order-9 md:order-8 items-center rounded-[2rem] bg-[#ececec] dark:bg-neutral-900 text-center text-neutral-600 dark:text-neutral-300 aspect-square"
     >
         <p className="lg:text-2xl font-medium">Proyectos</p>
-        <p className="lg:text-md font-light">pronto</p>
+        <p className="text-sm lg:text-md font-light">pronto</p>
     </a>
     )
 }
